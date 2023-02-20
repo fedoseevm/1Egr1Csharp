4b50437ad59d111ed7db76799d2810a87d7f2882
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2023-02-20 11:24:42 +0100

    Nie zrobione ostatnie zadania

---
+++ Tablice_zadanie.cs
@@ -0,0 +1,78 @@
+// 1. tablica z losowymi liczbami dwucyfrowymi nieparzystymi
+using System.Collections;
+
+Random r = new Random();
+//int[] L = new int[10];
+//for (int i = 0; i < L.Length; i++)
+//{
+//    L[i] = r.Next(5, 50) * 2 + 1;
+//    Console.Write(L[i] + " ");
+//}
+//Console.WriteLine();
+
+//foreach (var item in L)
+//{
+//    Console.Write(item + " ");
+//}
+//Console.WriteLine();
+//Console.WriteLine();
+
+// 2.
+//int[] T = new int[20];
+//for (int i = 0; i < T.Length; i++)
+//{
+//    T[i] = r.Next(10, 100);
+//    Console.Write(T[i] + " ");
+//    if (T[i] % 10 != 0)
+//    {
+//        T[i] = T[i] - (T[i] % 10) + 10;
+//    }
+//    Console.WriteLine(T[i]);
+//}
+//Console.WriteLine();
+
+// 3. wygeneruj liste 12 losowych liczb dwucyfrowych podzielnych przez 7
+//int[] N = new int[12];
+//for (int i = 0; i < N.Length; i++)
+//{
+//    N[i] = r.Next(2, 15) * 7;
+//    Console.Write(N[i] + " ");
+//}
+
+
+// 4. wygeneruj tablice 10 losowych liczb pierwszych
+// w1 - ArrayList
+// w2 - tablica
+
+//ArrayList A = new ArrayList();
+//int j = 10;
+//while (j > 0)
+//{
+//    bool flaga = true;
+//    int x = r.Next(10, 100);
+//    for (int i = 2; i < x / 2 + 1; i++)
+//    {
+//        if (x % i == 0)
+//        {
+//            flaga = false;
+//            break;
+//        }
+//    }
+//    if (flaga)
+//    {
+//        A.Add(x);
+//        j++;
+//    }
+//}
+//for (int i = 0; i < A.Count; i++)
+//{
+//    Console.Write(A[i] + " ");
+//}
+//Console.WriteLine();
+
+// 5. tablica 40 losowych liczb trzycyfrowych i wypisz sume na pozycjach nieprazystych 1, 3, 5 itd...
+int[] M = new int[80];
+for (int i = 0; i < M.Length; i++)
+{
+
+}
