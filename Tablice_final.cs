0c4c47eb53d24de80933b9ac2f47dedfdfb6f285
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2023-02-26 23:28:13 +0100

    Update Tablice_final.cs

--- Tablice_final.cs
+++ Tablice_final.cs
@@ -42,26 +42,30 @@ for (int i = 0; i < L.Length; i++)
 
 
 
-/*
+
 // Tablice 2D
+
 int[,] M = new int[2, 2];   // deklaracja
 M[0, 0] = 3;
 M[0, 1] = 9;   // definicja
-M[0, 2] = 6;
 M[1, 0] = 0;
 M[1, 1] = 7;
-M[1, 2] = 7;
-M[2, 0] = 1;
-M[2, 1] = 3;
-M[2, 2] = 8;
 
 int[,] N = new int[,] { { 2, 3, 4, 5 }, { 7, 8, 9, 10 }, { 16, 17, 18, 19 } };
+
 Console.WriteLine(N[1, 3]);
+Console.WriteLine(N[0, 1]);
+
 for (int i = 0; i < N.Length; i++)
 {
     Console.Write(N[i / N.GetLength(1), i % N.GetLength(1)] + " ");
 }
 Console.WriteLine("\n");
+
+
+// N.GetLength(0) -- zwraca ilosc podtablic w sobie, czyli w tym przypadku 3
+// N.GetLength(1) -- zwraca ilosc elementow w podtablicę, czyli w tym przypadku 4
+
 
 for (int i = 0; i < N.GetLength(0); i++)
 {
@@ -73,8 +77,8 @@ for (int i = 0; i < N.GetLength(0); i++)
 }
 Console.WriteLine("\n");
 
-
 // Losowe generowanie macierzy (tablicy dwuelementowej)
+/*
 int n = int.Parse(Console.ReadLine());
 int m = int.Parse(Console.ReadLine());
 Random r = new Random();
@@ -98,8 +102,8 @@ Console.WriteLine("\n");
 
 
 // List i ArrayList
-using System.Collections;
 /*
+using System.Collections;
 ArrayList AL = new ArrayList();
 AL.Add(5);
 AL.Add(8);
