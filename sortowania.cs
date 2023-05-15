7c47d1f16f22a46e303d8d87a2f2018d1be4ca4e
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2023-05-15 11:19:52 +0200

    Dodawanie QuickSort Hoare i Lomuto

--- sortowania.cs
+++ sortowania.cs
@@ -70,3 +70,86 @@ for (int i = 1; i < n; i++)
 
 
 // bucket
+
+
+
+
+
+// QuickSort Hoare
+List<int> quicksort_hoare(List<int> T, int left, int right)
+{
+    int i = left;
+    int j = right;
+    int pivot = T[(left + right) / 2];
+    while (i <= j)
+    {
+        while (T[i] < pivot)
+            i++;
+        while (T[j] > pivot)
+            j--;
+        if (i <= j)
+        {
+            int temp = T[i];
+            T[i] = T[j];
+            T[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    if (left < j)
+        T = quicksort_hoare(T, left, j);
+    if (right > i)
+        T = quicksort_hoare(T, i, right);
+    return T;
+}
+
+Random r = new Random();
+List<int> T = new List<int>();
+for (int i = 0; i < 10; i++)
+{
+    T.Add(r.Next(0,10));
+    Console.Write(T[i] + " ");
+}
+Console.WriteLine();
+quicksort_hoare(T, 0, T.Count - 1);
+for (int i = 0; i < 10; i++)
+    Console.Write(T[i] + " ");
+Console.WriteLine("\n");
+Console.ReadKey();
+
+
+
+// QuickSort Lomuto
+List<int> quicksort_lomuto(List<int> L, int left, int right)
+{
+    int pivot = L[right];
+    int i = left;
+    for (int k = left; k < right; k++)
+        if (L[k] <= pivot)
+        {
+            int temp = L[k];
+            L[k] = L[i];
+            L[i] = temp;
+            i++;
+        }
+    int temp1 = L[i];
+    L[i] = L[right];
+    L[right] = temp1;
+    if (left < i - 1)
+        T = quicksort_lomuto(L, left, i - 1);
+    if (right > i + 1)
+        T = quicksort_lomuto(L, i + 1, right);
+    return T;
+}
+
+
+List<int> L = new List<int>();
+for (int i = 0; i < 10; i++)
+{
+    L.Add(r.Next(0, 10));
+    Console.Write(L[i] + " ");
+}
+Console.WriteLine();
+quicksort_hoare(L, 0, L.Count - 1);
+for (int i = 0; i < 10; i++)
+    Console.Write(L[i] + " ");
